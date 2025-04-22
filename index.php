a494d257eabdf5448681765ba53c4427c14ee122
michael <ei.dwaps@gmail.com>
2025-04-22 08:42:25 -0400

    conditions

--- index.php
+++ index.php
@@ -1,67 +1,62 @@
 <?php
 
-// OPERATEURS
+// CONDITIONS
 
-// Affectation
-$a = 'a';
-// Opérateur combiné : affection élargie
-$nb1 = 6;
-$nb1 += 12; // --> $nb1 = $nb1 + 12;
-$nb1 /= 15;
-$nb1 *= 15;
-$nb1 -= 15;
-$nb1 %= 3;
-$nb1 **= 3;
+$mood = "content";
 
-// Opérateurs arythmétiques
-// + - / * % **
-$exp = 4 ** 2; // 16
+// If, elseif, else
+if ($mood === "content") {
+  echo "Tu es $mood", PHP_EOL;
+} elseif ($mood === "triste") {
+  echo "Quel dommage :(", PHP_EOL;
+} else {
+  echo "Quel est ton humeur ??", PHP_EOL;
+}
+?>
 
-// 10 % 2 = 0; // pair
-// 9 % 2 = 1; // impair
-// 8 % 2 = 0; // pair
-// 7 % 2 = 1; // impair
-// 6 % 2 = 0; // pair
-// 5 % 2 = 1; // impair
+<?php if ($mood === "content"): ?>
+  <p>Tu es <?= $mood ?></p>
+<?php elseif ($mood === "triste"): ?>
+  <p>Quel dommage :(</p>
+<?php else: ?>
+  <p>Quel est ton hummeur ??</p>
+<?php endif; ?>
 
-// Comparaison
-var_dump(5 == '5'); // true
-var_dump(5 === '5'); // false
-var_dump(5 != '5'); // false
-var_dump(5 !== '5'); // true
-var_dump(5 <> '5'); // false
+<?php
 
-// > < >= <=
+// switch
+switch ($mood) {
+  case 'content':
+    echo 'toi content !', PHP_EOL;
+    break;
+  case 'triste':
+  case 'maussade':
+    echo 'dommage', PHP_EOL;
+    break;
+  default:
+    echo 'ton humeur ??', PHP_EOL;
+}
 
-var_dump(5 <=> 5); // 0
-var_dump(3 <=> 5); // -1
-var_dump(8 <=> 5); // 1
+// match --> cas stricts
+$userChoice = "salade";
+$menu = match ($userChoice) {
+  "plat principal" => "raviolis",
+  "entrée", "salade" => "riz/thon/tomates",
+  default => "plat non servi",
+};
+echo $menu, PHP_EOL;
 
-// Opérateurs logiques
-// && || and or ! xor
-var_dump(true || true); // true
-var_dump(true or true); // true
-var_dump(true xor true); // false
+// match --> cas complexes et ternaire
+$age = 56;
+$accessAllowed = match (true) {
+  $age >= 18 => true,
+  default => false,
+};
+echo $accessAllowed ? "Accès autorisé" : "Accès interdit", PHP_EOL;
 
-// Opérateur d'exécution --> ``
-$output = `pwd`; // Script Shell
-echo $output, PHP_EOL;
-
-// Contrôle d'erreur
-echo @$none;
-
-// Opérateur peut être unaire, binaire ou ternaire
-// unaire --> agit sur une opérande
-$a = -1;
-$a++;
-// binaire --> agit sur deux opérandes
-$b = 2 + 4;
-// ternaire --> agit sur trois opérandes
-$c = 'coco' ? 'ok' : 'pas ok';
-
-// Précédence (priorité) des opérateurs
-var_dump(true || false && true || false && false);
-var_dump(true || false || false); // true
-// Associativité (ordre quand il y a la même priorité)
-echo 2 + 1 * 3; // 5
-echo (2 + 1) * 3; // 9
+// condition avec opérateur de fusion null
+// (coalescence des null)
+$username;
+$userConnected = $username ?? 'anonyme';
+$userConnected = isset($username) ? $username : 'anonyme';
+echo $userConnected, PHP_EOL;
