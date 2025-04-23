244fd1d75c8582b4630f31970387dc74b071529f
michael <ei.dwaps@gmail.com>
2025-04-23 08:40:56 -0400

    creation de l'index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <link rel="stylesheet" href="public/css/style.css">
+  <script defer src="public/js/index.js"></script>
+
+  <!-- GOOGLE FONT -->
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&display=swap" rel="stylesheet">
+
+  <title>ToraTaTache</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
